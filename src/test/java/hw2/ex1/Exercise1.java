ff64be362436356f0db01ec84e6cb95327c0a458
Khodakovsky, Dmitry <Dmitrii.Khodakovsky@Clarivate.com>
2019-06-23 21:04:13 +0300

    added comments for the hw2 #1

--- src/test/java/hw2/ex1/Exercise1.java
+++ src/test/java/hw2/ex1/Exercise1.java
@@ -19,9 +19,11 @@ public class Exercise1 extends BaseTest {
         assertBrowserTitle("Home Page");
 
         //3. Perform login
+        // TODO please look to the comment in the ConnectConstatnts enum
         login(USER_LOGIN.getData(), PASSWORD.getData());
 
         //4. Assert User name in the left-top side of screen that user is loggined
+        // TODO please look to the comment in the ConnectConstatnts enum
         assertEquals(driver.findElement(By.id("user-name")).getText(),USER.getData());
 
         //5. Assert Browser title
@@ -65,6 +67,7 @@ public class Exercise1 extends BaseTest {
         assertTrue(driver.findElement(By.className("footer-bg")).isDisplayed());
 
         //17. Close Browser
+        // TODO driver.close could be extracted to the AfterMethod hook
         driver.close();
     }
 }
