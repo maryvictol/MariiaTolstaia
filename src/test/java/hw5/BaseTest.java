a8121b77c8ca369a1062f371923f115d30fade60
Mariia_Tolstaia <Mariia_Tolstaia@epam.com>
2020-02-17 17:26:48 +0300

    fix

--- src/test/java/hw5/BaseTest.java
+++ src/test/java/hw5/BaseTest.java
@@ -8,13 +8,18 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
+import java.nio.file.Paths;
+
 public class BaseTest {
 
     protected WebDriver driver;
 
     @BeforeSuite
     public void setUpDriverPath() {
-        WebDriverManager.chromedriver().setup();
+//        WebDriverManager.chromedriver().setup();
+        System.setProperty("webdriver.chrome.driver",
+                Paths.get("src/test/resources/driver/chromedriver.exe")
+                        .toAbsolutePath().toString());
     }
 
     @BeforeMethod
